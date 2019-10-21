5a7be380bb838c1fc4488cbc709450d332478b86
Mariasola <mariasolahornedo@gmail.com>
2019-10-21 20:02:57 +0200

    first build docs

---
+++ docs/precache-manifest.2f4c9240633c2b5b6f08c8271838c3ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df12e552ef8734b1cb50c4aecc7ca1c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "07238e3c4f20dd5a01cd",
+    "url": "./static/css/main.422ff3a8.chunk.css"
+  },
+  {
+    "revision": "0ae4d4071ac7dfe562af",
+    "url": "./static/js/2.334f80c1.chunk.js"
+  },
+  {
+    "revision": "07238e3c4f20dd5a01cd",
+    "url": "./static/js/main.45c3cdec.chunk.js"
+  },
+  {
+    "revision": "024f6d866f9b19a53f2c",
+    "url": "./static/js/runtime-main.78058345.js"
+  },
+  {
+    "revision": "d0690374ab3b741bd9b95f3d30859248",
+    "url": "./static/media/background-1.d0690374.png"
+  }
+]);
